8caba811a39035300d1c0432de7ef0a5ce4d785c
fharlup <35958282+fharlup@users.noreply.github.com>
2024-03-16 16:40:39 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 ﻿using System;
 
+public class DataGeneric<T> where T : struct
+{
+    public T Data { get; private set; }
+
+    public void SetData(T data)
+    {
+        this.Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan: " + Data);
+    }
+}
+
 public class HaloGeneric<T> where T : class
 {
     public void SapaUser(T x)
@@ -14,5 +29,9 @@ public class Program
     {
         HaloGeneric<string> halo = new HaloGeneric<string>();
         halo.SapaUser("hallo praktian!");
-         }
+
+        DataGeneric<int> dataGeneric = new DataGeneric<int>();
+        dataGeneric.SetData(1302223032);
+        dataGeneric.PrintData();
+    }
 }
